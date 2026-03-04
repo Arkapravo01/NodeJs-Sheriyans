214f71e75435feedbee7c0745722eaf851f5f0df
Arkapravo <arkapravochakrabarti188@gmail.com>
2026-03-04 17:49:44 +0530

    6.00

--- Auth/src/app.js
+++ Auth/src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const authRoutes = require("./routes/auth.routes.js");
+const postRoutes = require("./routes/post.routes.js");
 const cookieParser = require("cookie-parser");
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/auth", postRoutes);
 
 module.exports = app;
