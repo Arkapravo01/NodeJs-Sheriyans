6c3f7432b6891d09da4f79b89e8f8bdc0b845e3e
Arkapravo <arkapravochakrabarti188@gmail.com>
2026-03-10 21:55:34 +0530

    Finished Finally!

--- jest/src/app.js
+++ jest/src/app.js
@@ -1,5 +1,29 @@
 const express = require("express");
 
 const app = express();
+const validationRules = require("./middlewares/validation.middleware");
+
+// app.get("/", (req, res) => {
+//   res.status(200).json({
+//     message: "Hello, World!",
+//   });
+// });
+app.use(express.json());
+
+app.post(
+  "/register",
+  validationRules.registerUserValidationRules,
+  (req, res) => {
+    const { userName, email, password } = req.body;
+
+    res.status(200).json({
+      message: "User registered successfully",
+      user: {
+        userName,
+        email,
+      },
+    });
+  },
+);
 
 module.exports = app;
